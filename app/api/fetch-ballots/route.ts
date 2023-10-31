fd0e221274f043622f6cd979b5ee90511e40c2c8
Jack Weru <werukioni@gmail.com>
2023-10-31 15:43:51 +0300

    completed stage1: landing page,  stage2: admin & user landing pages, stage3: user registration,  stage4: user session autehtication, stage5: create new ballot, stage6: show available ballots, stage7: handle voting in a ballot (...upcoming))

--- app/api/fetch-ballots/route.ts
+++ app/api/fetch-ballots/route.ts
@@ -1,20 +1,18 @@
 import BallotSchema from "@/models/ballotSchema";
 import { connectToDB } from "@/utils/database";
-import { NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
 
 
-export const GET = async(req: NextRequest, res: NextApiResponse) => {
+export const GET = async(req: NextRequest, res: NextResponse) => {
     try {
-        res.setHeader('Cache-Control', 'no-store');
         const isConnected = await connectToDB();
-
+        
         if (!isConnected) {
             throw new Error("Failed to connect to DB.");
         }
-
+        
         const ballots = await BallotSchema.find({});
-
+        
         return new NextResponse(JSON.stringify({ success: ballots}), { status: 200 });
     } catch (error) {
         console.log("Error: ", error);
