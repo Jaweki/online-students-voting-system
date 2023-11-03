596bcc1620fc4d20f9768be6b2a257ed640f4e0d
Jack Weru <werukioni@gmail.com>
2023-11-03 14:05:04 +0300

    caches will revalidate each minute.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
     images: {
         domains: ['res.cloudinary.com'],
     },
+    reactStrictMode: true,
+    async headers() {
+        return [{
+            source: '*',
+            headers: [
+                {
+                    key: 'Cache-Control',
+                    value: 's-maxage=1, stale-while-revalidate=59',
+                },
+            ]
+        }]
+    }
 }
 
 module.exports = nextConfig
